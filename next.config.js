b44a1432645aaaa2ddd10fe5fa1b44465894c12b
zowie <zowietao@gmail.com>
2023-02-27 16:53:09 +0800

    change js to ts, eslint config, prettier, pack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack:function (config, options) {
+    config.resolve.fallback = {
+      crypto: false,
+      path: false,
+      fs: false
+    };
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/i,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 1024 * 30,
+          name:'[path][name].[ext]',
+          encoding:'base64',
+          fallback: require.resolve('file-loader'),
+        }
+      }
+    })
+    return config
+  },
+  images: {
+    disableStaticImages: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
