6593f413dbd93a365f7c86a2943b55ca87ed275c
zowie <zowietao@gmail.com>
2023-04-14 17:54:11 +0800

    chore: jump test

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,29 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      hello world!
+      <>
+        <div>
+          <a href="https://www.baidu.com" target="_blank">
+            a tag to baidu:{`<a href="https://www.baidu.com" target="_blank">`}
+          </a>
+        </div>
+        <div style={{ height: '30px' }} />
+        <div
+          onClick={() => {
+            window?.open('https://www.baidu.com', '_blank');
+          }}
+        >
+          {`click window?.open('https://www.baidu.com', '_blank');`}
+        </div>
+        <div style={{ height: '30px' }} />
+        <div
+          onClick={() => {
+            window.location.href = 'https://www.baidu.com';
+          }}
+        >
+          {`click window.location.href = 'https://www.baidu.com';`}
+        </div>
+      </>
     </>
   );
 }
