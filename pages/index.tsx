8f773fbb49dd71109d4d2070c3009d925721124e
zowie <zowietao@gmail.com>
2023-04-17 19:06:49 +0800

    feat: add await

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
+import './global.css';
+
 import Head from 'next/head';
+
+import { wait } from '@/util';
 
 export default function Home() {
   return (
@@ -11,25 +15,69 @@ export default function Home() {
       </Head>
       <>
         <div>
-          <a href="https://www.baidu.com" target="_blank">
-            a tag to baidu:{`<a href="https://www.baidu.com" target="_blank">`}
+          <a href="https://www.baidu.com/s?wd=hello" target="_blank">
+            a tag to baidu:
+            {`<a href="https://www.baidu.com/s?wd=hello" target="_blank">`}
           </a>
         </div>
+
         <div style={{ height: '30px' }} />
+
         <div
           onClick={() => {
-            window?.open('https://www.baidu.com', '_blank');
+            const aTag = document.createElement('a');
+            aTag.setAttribute('href', 'https://www.baidu.com/s?wd=hello');
+            aTag.setAttribute('target', '_blank');
+            aTag.setAttribute('download', 'name');
+            document.body.appendChild(aTag);
+            aTag.click();
+            aTag.remove();
           }}
         >
-          {`click window?.open('https://www.baidu.com', '_blank');`}
+          create element to simulator a tag to baidu:
+          {`<a href="https://www.baidu.com/s?wd=hello" target="_blank">`}
         </div>
+
         <div style={{ height: '30px' }} />
+
         <div
           onClick={() => {
-            window.location.href = 'https://www.baidu.com';
+            window?.open('https://www.baidu.com/s?wd=hello', '_blank');
           }}
         >
-          {`click window.location.href = 'https://www.baidu.com';`}
+          {`click window?.open('https://www.baidu.com/s?wd=hello', '_blank');`}
+        </div>
+
+        <div style={{ height: '30px' }} />
+
+        <div
+          onClick={async () => {
+            await wait(1000);
+            window?.open('https://www.baidu.com/s?wd=hello', '_blank');
+          }}
+        >
+          {`click ASYNC to window?.open('https://www.baidu.com/s?wd=hello', '_blank');`}
+        </div>
+
+        <div style={{ height: '30px' }} />
+
+        <div
+          onClick={() => {
+            window.location.href = 'https://www.baidu.com/s?wd=hello';
+          }}
+        >
+          {`click window.location.href = 'https://www.baidu.com/s?wd=hello';`}
+        </div>
+
+        <div style={{ height: '30px' }} />
+
+        <div
+          onClick={async () => {
+            await wait(1000);
+            window.location.href = 'https://www.baidu.com/s?wd=hello';
+          }}
+        >
+          {`click ASYNC window.location.href = 'https://www.baidu.com/s?wd=hello';`}
         </div>
       </>
     </>
